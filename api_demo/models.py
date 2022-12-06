a5de88748eb49cb9e48935c8da7078dfe5b31003
kyrylokireiev <kyrylo.kireiev@yalantis.net>
2022-12-06 17:22:03 +0200

    add BlogBase class with id and pud date to model

--- api_demo/models.py
+++ api_demo/models.py
@@ -3,21 +3,25 @@ from sqlalchemy import (Column, ForeignKey,
                         )
 
 from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
 
 Base = declarative_base()
 
 
-class Article(Base):
-    __tablename__ = 'article'
+class BlogBase(object):
     id = Column(Integer, primary_key=True)
+    pub_date = Column(DateTime, default=datetime.utcnow(), nullable=False)
+
+
+class Article(BlogBase, Base):
+    __tablename__ = 'article'
     article_name = Column(String(200), nullable=False)
     article_text = Column(Text, nullable=False)
     likes = Column(Integer, default="0", nullable=False)
     category_id = Column(Integer, ForeignKey('category.id', ondelete='CASCADE'))
 
 
-class Category(Base):
+class Category(BlogBase, Base):
     __tablename__ = 'category'
-    id = Column(Integer, primary_key=True)
     category_name = Column(String(100), nullable=False)
-    pub_date = Column(DateTime, nullable=False)
+
