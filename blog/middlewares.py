62491038156728829ad3e07e54d06d9efb01ec98
yuniia.olkhova <yuniia.olkhova@yalantis.net>
2022-12-23 15:29:12 +0200

    Refactor error response

--- blog/middlewares.py
+++ blog/middlewares.py
@@ -8,9 +8,8 @@ from marshmallow import ValidationError
 async def error_middleware(request, handler):
     try:
         return await handler(request)
-    except ValidationError as ex:
-        response_obj = {"error": str(ex)}
-        return web.Response(text=json.dumps(response_obj))
+    except ValidationError as error:
+        return web.Response(text=str(error))
 
 
 def setup_middlewares(app):
