0045ef203d93c278c746ef50b60b0a1e1bfb087d
yuniia.olkhova <yuniia.olkhova@yalantis.net>
2022-12-23 15:27:18 +0200

    Refactor middleware

--- blog/middlewares.py
+++ blog/middlewares.py
@@ -4,23 +4,17 @@ from aiohttp import web
 from marshmallow import ValidationError
 
 
-def create_error_middleware():
-    @web.middleware
-    async def error_middleware(request, handler):
-        try:
-            return await handler(request)
-
-        except web.HTTPException as ex:
-            response_obj = {"error": str(ex.text)}
-            return web.Response(text=json.dumps(response_obj))
-
-        except ValidationError as ex:
-            response_obj = {"error": str(ex)}
-            return web.Response(text=json.dumps(response_obj))
-
-    return error_middleware
+@web.middleware
+async def error_middleware(request, handler):
+    try:
+        return await handler(request)
+    except web.HTTPException as ex:
+        response_obj = {"error": str(ex.text)}
+        return web.Response(text=json.dumps(response_obj))
+    except ValidationError as ex:
+        response_obj = {"error": str(ex)}
+        return web.Response(text=json.dumps(response_obj))
 
 
 def setup_middlewares(app):
-    error_middleware = create_error_middleware()
     app.middlewares.append(error_middleware)
