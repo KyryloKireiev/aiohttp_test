5f953e13e59a7a8b061ff2791d3e5c3519715cfb
kyrylokireiev <kyrylo.kireiev@yalantis.net>
2023-01-09 22:10:39 +0200

    discard changes

--- blog/utils.py
+++ blog/utils.py
@@ -1,14 +1,7 @@
-"""def generate_hash(password):
+import bcrypt
+
+
+def generate_hash(password):
     password_bin = password.encode("utf-8")
     hashed = bcrypt.hashpw(password_bin, bcrypt.gensalt())
-    return hashed.decode("utf-8")"""
-
-
-def get_test_merge_conflict():
-    print("Try to make conflict")
-
-
-# def generate_hash(password):
-#     password_bin = password.encode("utf-8")
-#     hashed = bcrypt.hashpw(password_bin, bcrypt.gensalt())
-#     return hashed.decode("utf-8")
+    return hashed.decode("utf-8")
