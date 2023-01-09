ca462f11456efe7eba444ae8db56b63611972c4e
kyrylokireiev <kyrylo.kireiev@yalantis.net>
2023-01-09 22:12:47 +0200

    try to make conflict

--- blog/utils.py
+++ blog/utils.py
@@ -1,6 +1,10 @@
 import bcrypt
 
 
+def get_add_conflict():
+    print("try to add conflict to merge")
+
+
 def generate_hash(password):
     password_bin = password.encode("utf-8")
     hashed = bcrypt.hashpw(password_bin, bcrypt.gensalt())
